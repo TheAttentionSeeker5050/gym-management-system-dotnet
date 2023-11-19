e140762b668be5220bf3f1a9991a51f2101c4063
nicolas <nicolas.castellano.6@hotmail.com>
2023-11-19 18:37:53 -0400

    added user session authentication

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using gym_management_system.Models;
 using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 
 namespace gym_management_system.Controllers
 {
@@ -27,7 +33,7 @@ namespace gym_management_system.Controllers
 
         [HttpPost]
         [Route("login")]
-        public IActionResult LoginSubmit(User user)
+        public async Task<IActionResult> LoginSubmitAsync(User user)
         {
 
             try
@@ -35,8 +41,33 @@ namespace gym_management_system.Controllers
                 // attempt login the user with the login model method. and user credentials in form
                 user = user.loginUser(user);
 
-                // now add the user to the session
-                
+                // session mapping ------------------------------------------
+                // map user claims
+                var claims = new List<Claim> { 
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+                };
+
+                // claims identity
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                    IsPersistent = true,
+                    RedirectUri = "/"
+                };
+
+                // sign in the user to the cookie authentication scheme
+                // using session authentication
+                // method is asynchronous, so await it
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                // add log to the console
+                Console.WriteLine("User logged in: " + user.Email + "in at " + DateTime.Now);
+
+
             } catch (Exception e)
             {
                 TempData["error"] = e.Message;
@@ -45,7 +76,7 @@ namespace gym_management_system.Controllers
 
             // if login is successful, redirect to the dashboard
             TempData["feedback"] = "Login successful";
-            return RedirectToAction("Login");
+            return RedirectToAction("Index", "Home");
             // return RedirectToAction("Index", "Home");
         }
 
