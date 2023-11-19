859b668034e9056c8f1403dc68e4c5e8900893d3
nicolas <nicolas.castellano.6@hotmail.com>
2023-11-19 18:02:01 -0400

    added password hashing

--- Models/User.DataModel.cs
+++ Models/User.DataModel.cs
@@ -2,10 +2,15 @@
 using MongoDB.Driver;
 using MongoDB.Bson;
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using gym_management_system.Models;
+
+// cryptography using BCrypt
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
 
 namespace gym_management_system.Models
 {
@@ -13,8 +18,14 @@ namespace gym_management_system.Models
 
     public class User
     {
-
+        
         private DBConnection _connection = new DBConnection();
+
+
+        // private constants --------------------------------------------
+        private byte[] encryption_salt;
+
+
         // constructor
         public User() {
             // make id a mongo ObjectId
@@ -23,6 +34,10 @@ namespace gym_management_system.Models
             _userPassword = "";
             _email = "";
             _fullName  = "";
+
+            string env_salt = Environment.GetEnvironmentVariable("HASH_SECRET_SALT").ToString();
+
+            encryption_salt = Encoding.ASCII.GetBytes(env_salt);
 
         }
 
@@ -51,46 +66,37 @@ namespace gym_management_system.Models
                 var db = client.GetDatabase("gym_management_system");
                 var collection = db.GetCollection<User>("users");
                 // collection.InsertOne(user);
-                // Console.WriteLine("User created");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+                throw new Exception("Something went wrong: Could not create user");
             }
         }
 
         public void loginUser(User user)
         {
+            User userResult = new User();
             try
             {
-                Console.WriteLine("Mongo connection string: " + _connection.MONGO_CONN_STRING);
-
+                
                 // initialize the mongo client and get the database and collection
                 var client = new MongoClient(_connection.MONGO_CONN_STRING);
                 var db = client.GetDatabase("gym_management_system");
                 var collection = db.GetCollection<User>("users");
 
-                Console.WriteLine("User email: " + user.Email);
 
                 // add filters fpr searching the user
                 var filter = Builders<User>.Filter.Eq("Email", user.Email);
-                var userResult = collection.Find(filter).FirstOrDefault();
+                userResult = collection.Find(filter).FirstOrDefault();
 
-                Console.WriteLine("User result: " + userResult);
 
                 if (userResult.Id == null)
                 {
                     throw new Exception("Incorrect user credentials");
                 }
-                else
-                {
-                    if (userResult.UserPassword != user.UserPassword)
-                    {
-                        throw new Exception("Incorrect user credentials");
-                    }
-                }
 
-                Console.WriteLine("User logged in");
+                
             }
             catch (Exception e)
             {
@@ -98,10 +104,74 @@ namespace gym_management_system.Models
                 throw new Exception("Something went wrong: Could not log in user");
             }
 
+            try
+            {
+                // now compare the passwords using the method compare passwords
+                bool passwordsMatch = ComparePasswords(user.UserPassword, userResult.UserPassword);
+
+                if (passwordsMatch == false)
+                {
+                    Console.WriteLine("Passwords do not match");
+                    throw new Exception("Incorrect user credentials");
+                }
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new Exception(e.Message);
+            }
+
+
         }
 
 
+        public string hashPassword(string password)
+        {
+            try
+            {
 
+                // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+                string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                    password: password!,
+                    salt: encryption_salt,
+                    prf: KeyDerivationPrf.HMACSHA256,
+                    iterationCount: 100000,
+                    numBytesRequested: 256 / 8));
+
+                return hashed;
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new Exception("Something went wrong: Could not hash password");
+            }
+        }
+
+        public bool ComparePasswords(string inPassword, string hashedDBPassword)
+        {
+            // this method compares the password entered by the user (inPassword) with the hashed password in the database hashedDBPassword
+            try
+            {
+
+                string hashedInPassword = hashPassword(inPassword);
+
+                // compare the two hashed passwords, return true if they are the same, false if not
+                if (hashedInPassword == hashedDBPassword)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new Exception("Something went wrong: Could not hash password");
+            }
+        }
 
 
     }
