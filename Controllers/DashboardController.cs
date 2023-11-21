07e8a475d181625d0ae9cd2eb311655621a50cd6
nicolas <nicolas.castellano.6@hotmail.com>
2023-11-20 21:20:14 -0400

    strip extra code on auth views

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gym_management_system.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+
+            _logger = logger;
+        }
+
+        // GET: DashboardController
+        [HttpGet]
+        [Route("dashboard")]
+        public ActionResult Index()
+        {
+            // if user is not logged in, redirect to login page
+            if (User.Identity.IsAuthenticated == false)
+            {
+                ViewBag.IsAuthenticated = false;
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.IsAuthenticated = true;
+            return View();
+        }
+
+    }
+}
