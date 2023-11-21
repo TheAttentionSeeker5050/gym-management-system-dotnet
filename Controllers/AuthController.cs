9c7b5d9afa23b561dfc7f220e8bfac1df1fea522
nicolas <nicolas.castellano.6@hotmail.com>
2023-11-20 20:46:03 -0400

    added register action and model

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -33,7 +33,7 @@ namespace gym_management_system.Controllers
 
         [HttpPost]
         [Route("login")]
-        public async Task<IActionResult> LoginSubmitAsync(UserLogin user)
+        public IActionResult LoginSubmit(UserLogin user)
         {
             // verify model state form is valid
             if (!ModelState.IsValid)
@@ -47,27 +47,8 @@ namespace gym_management_system.Controllers
                 user = user.loginUser(user);
 
                 // session mapping ------------------------------------------
-                // map user claims
-                var claims = new List<Claim> { 
-                    new Claim(ClaimTypes.Email, user.Email),
-                    new Claim(ClaimTypes.Name, user.UserName),
-                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
-                };
-
-                // claims identity
-                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-                var authProperties = new AuthenticationProperties
-                {
-                    AllowRefresh = true,
-                    IsPersistent = true,
-                    RedirectUri = "/"
-                };
-
-                // sign in the user to the cookie authentication scheme
-                // using session authentication
-                // method is asynchronous, so await it
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+                // use the map method to map user claims
+                MapUserClaims(user);
 
                 // add log to the console
                 Console.WriteLine("User logged in: " + user.Email + " in at " + DateTime.Now);
@@ -90,32 +71,125 @@ namespace gym_management_system.Controllers
         [Route("register")]
         public IActionResult Register()
         {
+            // check feedback and error messages from the login form
+            if (TempData["feedback"] != null)
+            {
+                ViewBag.feedback = TempData["feedback"];
+            }
+            if (TempData["error"] != null)
+            {
+                ViewBag.error = TempData["error"];
+            }
+
             return View();
         }
 
         [HttpPost]
         [Route("register")]
-        public IActionResult RegisterSubmit()
+        public IActionResult RegisterSubmit(UserRegister user)
         {
-            return View("Register");
+
+            // verify model state form is valid
+            if (!ModelState.IsValid)
+            {
+                return View("Register");
+            }
+
+            // create a new user object
+            try
+            {
+                Console.WriteLine("Before user create function");
+                // attempt to register the user with the register model method
+                user.createUser(user);
+
+                // add log to the console
+                Console.WriteLine("User registered: " + user.Email + " in at " + DateTime.Now);
+
+            } catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+                return RedirectToAction("Register");
+            }
+
+            // if registration is successful, redirect to the main home page
+            return RedirectToAction("Login");
         }
 
         [HttpGet]
         [Route("guest-login")]
         public IActionResult GuestLogin()
         {
-            return View();
+
+            // use the guest login model method to login the user
+
+            UserLogin user = new UserLogin();
+
+            user = user.guestLogin();
+
+            // session mapping ------------------------------------------
+            // map user claims
+
+            // use the map method to map user claims
+            MapUserClaims(user);
+            
+            // redirect to the home index page
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpGet]
         [Route("logout")]
         public IActionResult Logout( )
         {
+
+            // unmap user claims using the unmap method
+            UnmapUserClaims();
+
+            /*// sign out the user from the cookie authentication scheme
+            // using session authentication
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);*/
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        // private methods --------------------------------------------
+        // map method to map user claims
+        private ClaimsPrincipal MapUserClaims(UserLogin user)
+        {
+            // map user claims
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+
+            // claims identity
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                AllowRefresh = true,
+                IsPersistent = true,
+                RedirectUri = "/"
+            };
+
+            // sign in the user to the cookie authentication scheme
+            // using session authentication
+            // method is asynchronous, so await it
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+            
+
+            return new ClaimsPrincipal(claimsIdentity);
+        }
+
+        // unmap method to unmap user claims and sign out the user
+        private void UnmapUserClaims()
+        {
             // sign out the user from the cookie authentication scheme
             // using session authentication
             HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
-            return RedirectToAction("Index", "Home");
         }
 
         
