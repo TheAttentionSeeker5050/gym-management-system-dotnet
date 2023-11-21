3d4054ec06d8d058401f1ac126fa35bcbd5252ec
nicolas <nicolas.castellano.6@hotmail.com>
2023-11-20 21:33:20 -0400

    starting branch

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gym_management_system.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Controllers/MemberController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gym_management_system.Controllers
+{
+    public class MemberController : Controller
+    {
+
+        private readonly ILogger<MemberController> _logger;
+
+        public MemberController(ILogger<MemberController> logger)
+        {
+
+            _logger = logger;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
